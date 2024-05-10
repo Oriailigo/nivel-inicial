79669370a03b8d311face691bac9963005fb28c7
ori <ori_larusa@hotmail.com>
2024-05-10 02:57:16 -0300

    agregue video de intro

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 // Espera a que el DOM esté completamente cargado
 document.addEventListener("DOMContentLoaded", function() {
+  var loadingVideo = document.getElementById("loading-video");
+  var content = document.getElementById("mostrarContenido");
+
     // Temporizador para ocultar el video después de 10 segundos
     setTimeout(function() {
-      var loadingVideo = document.getElementById("loading-video");
-      var content = document.getElementById("mostrarContenido");
+
       loadingVideo.style.display = "none";
       content.style.display = "block";
-    }, 5000); // 10000 milisegundos = 10 segundos
+
+    }, 11000); // 10000 milisegundos = 10 segundos
+
+
   });
+  
   
